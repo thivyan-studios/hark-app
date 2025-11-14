c417a611365b31aa24ffcc09895b89a9f63dd95c
thivyan-studios <thivyanstudios@gmail.com>
2025-11-14 23:24:45 +0200

    Added methods for donations :)

--- app/src/main/java/com/thivyanstudios/hark/screens/SettingsScreen.kt
+++ app/src/main/java/com/thivyanstudios/hark/screens/SettingsScreen.kt
@@ -1,28 +1,37 @@
 package com.thivyanstudios.hark.screens
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun SettingsScreen(versionName: String) {
-    Box(
+    val uriHandler = LocalUriHandler.current
+
+    Column(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Settings Screen")
+        Button(onClick = { uriHandler.openUri("https://ko-fi.com/thivyanstudios") }) {
+            Text(text = "Support me on Ko-fi")
+        }
+    }
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Bottom) {
         Text(
             text = versionName,
             color = MaterialTheme.colorScheme.onSurfaceVariant,
             style = MaterialTheme.typography.bodyMedium,
             modifier = Modifier
-                .align(Alignment.BottomCenter)
                 .padding(bottom = 16.dp)
         )
     }
