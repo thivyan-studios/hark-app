de48d02a0aba52e4fa7c73fefe94a899358e14bb
thivyan-studios <thivyanstudios@gmail.com>
2025-11-14 14:02:09 +0200

    Cleaning up assets :)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 23
         targetSdk = 36
         versionCode = 1
-        versionName = "0.2.4"
+        versionName = "0.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
