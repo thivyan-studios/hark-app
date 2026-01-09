ea9421d5730e932e60956ecccbd09f0ce106f918
thivyan-studios <thivyanstudios@gmail.com>
2026-01-09 16:55:34 +0200

    Continuing to migrate to C++

--- app/src/main/java/com/thivyanstudios/hark/audio/stream/AudioSource.kt
+++
@@ -1,9 +0,0 @@
-package com.thivyanstudios.hark.audio.stream
-
-interface AudioSource {
-    val audioSessionId: Int
-    fun start()
-    fun stop()
-    fun release()
-    fun read(audioData: FloatArray, offsetInFloats: Int, sizeInFloats: Int, readMode: Int): Int
-}
